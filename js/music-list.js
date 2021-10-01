65ae4073e733f9d9c3d3199113beafffc9849512
Najmul Alam Miraj <najmulalammiraj@gmail.com>
2021-10-02 01:07:16 +0600

    first commit

--- js/music-list.js
+++ js/music-list.js
@@ -11,89 +11,690 @@
 
 let allMusic = [
   {
-    name: "Tomar Premer Sur",
-    artist: "Mamunur Rashid",
-    img: "music-00",
-    src: "music-00"
-  },
-  {
-    name: "Mera Pyara Nabi",
-    artist: "Mamunur Rashid",
-    img: "music-0",
-    src: "music-0"
-  },
-  {
-    name: "Muhammad Rasul (SM:)",
-    artist: "Sadikur Rahman , Mushir Alom Mijan",
+    name: "Surah Al-Fatihah",
+    artist: "Mishary Rashid Alafasy",
     img: "music-1",
     src: "music-1"
   },
   {
-    name: "Paina Khuja Sur",
-    artist: "Sheikh Afzal Hussain Sadi",
-    img: "music-2",
+    name: "Surah Al-Baqara",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-2"
   },
   {
-    name: "Rasul Namer",
-    artist: "Sheikh Afzal Hussain Sadi",
-    img: "music-3",
+    name: "Surah Al-Imran",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-3"
   },
   {
-    name: "Marhaba Ya Mustafa",
-    artist: "Mushir Alom Mijan , Sayeem Ahmod Emon",
-    img: "music-4",
+    name: "Surah An-Nisaa",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-4"
   },
   {
-    name: "Meherban",
-    artist: "Zakaria Ahmad Shahan",
-    img: "music-5",
+    name: "Surah Al-Maidah",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-5"
   },
   {
-    name: "Allahu Allah",
-    artist: "Anwar Hussain Gulzar",
-    img: "music-6",
+    name: "Surah Al-An'am",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-6"
   },
   {
-    name: "Satto Pather Disa",
-    artist: "Sheikh Afzal Hussain Sadi",
-    img: "music-7",
+    name: "Surah Al-A'raf",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-7"
   },
   {
-    name: "Mustofa Mujtoba",
-    artist: "Sheikh Afzal Hussain Sadi",
-    img: "music-8",
+    name: "Surah Al-Anfal",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-8"
   },
   {
-    name: "Ami Nisite",
-    artist: "Sheikh Afzal Hussain Sadi",
-    img: "music-9",
+    name: "Surah At-Taubah",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-9"
   },
   {
-    name: "Tumer Dedar Cheya",
-    artist: "Sheikh Afzal Hussain Sadi",
-    img: "music-10",
+    name: "Surah Yunus",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-10"
   },
   {
-    name: "Allahu Allahu ",
-    artist: "Mushir Alom Mijan , Sayeem Ahmod Emon",
-    img: "music-11",
+    name: "Surah Hud",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-11"
   },
   {
-    name: "Madiney Ki Gali ",
-    artist: "Mushir Alom Mijan , Sayeem Ahmod Emon",
-    img: "music-12",
+    name: "Surah Yusuf",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
     src: "music-12"
   },
+  {
+    name: "Surah Ar-Ra'd",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-13"
+  },
+  {
+    name: "Surah Ibrahim",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-14"
+  },
+  {
+    name: "Surah Al-Hijr",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-15"
+  },
+  {
+    name: "Surah An-Nahl",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-16"
+  },
+  {
+    name: "Surah Israel",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-17"
+  },
+  {
+    name: "Surah Al-Kahf",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-18"
+  },
+  {
+    name: "Surah Maryam",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-19"
+  },
+  {
+    name: "Surah Ta-ha",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-20"
+  },
+  {
+    name: "Surah Al-Anbiyaa",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-21"
+  },
+  {
+    name: "Surah Al-Hajj",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-22"
+  },
+  {
+    name: "Surah Al-Muminun",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-23"
+  },
+  {
+    name: "Surah An-Nur",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-24"
+  },
+  {
+    name: "Surah Al-Furqan",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-25"
+  },
+  {
+    name: "Surah Ash-Shu'araa",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-26"
+  },
+  {
+    name: "Surah An-Naml",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-27"
+  },
+  {
+    name: "Surah Al-Qasas",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-28"
+  },
+  {
+    name: "Surah Al-Ankabut",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-29"
+  },
+  {
+    name: "Surah Ar-Rum",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-30"
+  },
+  {
+    name: "Surah Luqman",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-31"
+  },
+  {
+    name: "Surah As-Sajda",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-32"
+  },
+  {
+    name: "Surah Al-Ahzab",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-33"
+  },
+  {
+    name: "Surah Saba",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-34"
+  },
+  {
+    name: "Surah Fatir",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-35"
+  },
+  {
+    name: "Surah Ya-Sin",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-36"
+  },
+  {
+    name: "Surah As-Saffat",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-37"
+  },
+  {
+    name: "Surah Sad",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-38"
+  },
+  {
+    name: "Surah Az-Zumar",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-39"
+  },
+  {
+    name: "Surah Al-Mu'min",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-40"
+  },
+  {
+    name: "Surah Ha-Mim",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-41"
+  },
+  {
+    name: "Surah Ash-Shura",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-42"
+  },
+  {
+    name: "Surah Az-Zukhruf",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-43"
+  },
+  {
+    name: "Surah Ad-Dukhan",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-44"
+  },
+  {
+    name: "Surah Al-Jathiya",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-45"
+  },
+  {
+    name: "Surah Al-Ahqaf",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-46"
+  },
+  {
+    name: "Surah Muhammad",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-47"
+  },
+  {
+    name: "Surah Al-Fat-h",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-48"
+  },
+  {
+    name: "Surah Al-Hujurat",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-49"
+  },
+  {
+    name: "Surah Qaf",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-50"
+  },
+  {
+    name: "Surah Az-Zariyat",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-51"
+  },
+  {
+    name: "Surah At-Tur",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-52"
+  },
+  {
+    name: "Surah An-Najm",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-53"
+  },
+  {
+    name: "Surah Al-Qamar",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-54"
+  },
+  {
+    name: "Surah Ar-Rahman",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-55"
+  },
+  {
+    name: "Surah Al-Waqi'a",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-56"
+  },
+  {
+    name: "Surah Al-Hadid",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-57"
+  },
+  {
+    name: "Surah Al-Mujadila",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-58"
+  },
+  {
+    name: "Surah Al-Hashr",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-59"
+  },
+  {
+    name: "Surah Al-Mumtahana",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-60"
+  },
+  {
+    name: "Surah As-Saff",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-61"
+  },
+  {
+    name: "Surah Al-Jumu'a",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-62"
+  },
+  {
+    name: "Surah Al-Munafiqun",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-63"
+  },
+  {
+    name: "Surah At-Tagabun",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-64"
+  },
+  {
+    name: "Surah At-Talaq",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-65"
+  },
+  {
+    name: "Surah At-Tahrim",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-66"
+  },
+  {
+    name: "Surah Al-Mulk",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-67"
+  },
+  {
+    name: "Surah Al-Qalam",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-68"
+  },
+  {
+    name: "Surah Al-Haqqa",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-69"
+  },
+  {
+    name: "Surah Al-Ma'arij",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-70"
+  },
+  {
+    name: "Surah Nuh",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-71"
+  },
+  {
+    name: "Surah Al-Jinn",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-72"
+  },
+  {
+    name: "Surah Al-Muzzammil",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-73"
+  },
+  {
+    name: "Surah Al-Muddathth",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-74"
+  },
+  {
+    name: "Surah Al-Qiyamat",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-75"
+  },
+  {
+    name: "Surah Ad-Dahr",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-76"
+  },
+  {
+    name: "Surah Al-Mursalat",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-77"
+  },
+  {
+    name: "Surah An-Nabaa",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-78"
+  },
+  {
+    name: "Surah An-Nazi'at",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-79"
+  },
+  {
+    name: "Surah Abasa",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-80"
+  },
+  {
+    name: "Surah At-Takwir",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-81"
+  },
+  {
+    name: "Surah Al-Infitar",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-82"
+  },
+  {
+    name: "Surah Al-Mutaffife",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-83"
+  },
+  {
+    name: "Surah Al-Inshiqaq",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-84"
+  },
+  {
+    name: "Surah Al-Buruj",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-85"
+  },
+  {
+    name: "Surah At-Tariq",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-86"
+  },
+  {
+    name: "Surah Al-A'la",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-87"
+  },
+  {
+    name: "Surah Al-Gashiya",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-88"
+  },
+  {
+    name: "Surah Al-Fajr",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-89"
+  },
+  {
+    name: "Surah Al-Balad",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-90"
+  },
+  {
+    name: "Surah Ash-Shams",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-91"
+  },
+  {
+    name: "Surah Al-Lail",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-92"
+  },
+  {
+    name: "Surah Adh-Dhuha",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-93"
+  },
+  {
+    name: "Surah Al-Sharh",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-94"
+  },
+  {
+    name: "Surah At-Tin",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-95"
+  },
+  {
+    name: "Surah Al-Alaq",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-96"
+  },
+  {
+    name: "Surah Al-Qadr",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-97"
+  },
+  {
+    name: "Surah Al-Baiyina",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-98"
+  },
+  {
+    name: "Surah Al-Zalzalah",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-99"
+  },
+  {
+    name: "Surah Al-Adiyat",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-100"
+  },
+  {
+    name: "Surah Al-Qari'a",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-101"
+  },
+  {
+    name: "Surah At-Takathur",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-102"
+  },
+  {
+    name: "Surah Al-Asr",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-103"
+  },
+  {
+    name: "Surah Al-Humaza",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-104"
+  },
+  {
+    name: "Surah Al-Fil",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-105"
+  },
+  {
+    name: "Surah Quraish",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-106"
+  },
+  {
+    name: "Surah Al-Ma'un",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-107"
+  },
+  {
+    name: "Surah Al-Kauthar",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-108"
+  },
+  {
+    name: "Surah Al-Kafirun",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-109"
+  },
+  {
+    name: "Surah An-Nasr",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-110"
+  },
+  {
+    name: "Surah Al-Lahab",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-111"
+  },
+  {
+    name: "Surah Al-Ikhlas",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-112"
+  },
+  {
+    name: "Surah Al-Falaq",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-113"
+  },
+  {
+    name: "Surah Al-Nas",
+    artist: "Mishary Rashid Alafasy",
+    img: "music-1",
+    src: "music-114"
+  },
+
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
